fe7d13f8392dad7d4c9ca0ac2f107df12386b46f
claudio22fl <105253477+claudio22fl@users.noreply.github.com>
2023-09-02 18:53:21 -0400

    Update useDaraForm.tsx

--- src/app/Installation/hooks/useDaraForm.tsx
+++ src/app/Installation/hooks/useDaraForm.tsx
@@ -13,7 +13,6 @@ export const useDataForm = () => {
   date1.setMonth(date1.getMonth() + Number(1));
 
   const [formData, setFormData] = useState<fotmatAttributes>({
-    id: 0,
     fecha: formatDateInputs(new Date().toString()),
     hours: formatHourInputs(new Date().toString()),
     installer: "",
