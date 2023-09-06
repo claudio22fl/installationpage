e5ed68ed4d2ce52945fdf0362ba1b23b869dbf20
claudio22fl <claudio22fl@gmail.com>
2023-09-06 16:35:49 -0300

    update list

--- src/app/Installation_grafic/component/Table.tsx
+++ src/app/Installation_grafic/component/Table.tsx
@@ -74,7 +74,7 @@ export default function CollapsibleTable({
       newPagination.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
     );
 
-  }, [page, rowsPerPage, empresas, Pagination]);
+  }, [page, rowsPerPage, empresas, newPagination]);
 
   useEffect(() => {
     setNewPagination([...empresas]);
