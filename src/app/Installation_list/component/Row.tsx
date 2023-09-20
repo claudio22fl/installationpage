8cde3a94dc040b45e9b3036c0e9ec1880b142ce8
claudio22fl <claudio22fl@gmail.com>
2023-09-20 11:45:14 -0300

    update list

--- src/app/Installation_list/component/Row.tsx
+++ src/app/Installation_list/component/Row.tsx
@@ -110,7 +110,7 @@ export function Row({ row, client, fetchInstalattion }: Props) {
         </TableCell>
       </TableRow>
       <TableRow>
-        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={12}>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={13}>
           <Collapse in={open} timeout="auto" unmountOnExit>
             <Box sx={{ margin: 1 }}>
               <Typography variant="h6" gutterBottom component="div">
